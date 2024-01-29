78896972d0f6c3cbc2be4ac896bfe35f5ec1c977
Dejan Gaspar <107142760+Gaspardious@users.noreply.github.com>
2024-01-29 19:35:18 +0100

    script.js

---
+++ Utbildning/YH/JS-DOM-manipulation/index.js
@@ -0,0 +1,21 @@
+
+/*
+let title = document.getElementById("title");
+
+window.addEventListener ('load', changeText) 
+
+function changeText() {
+    title.innerHTML = "Uppdaterad!"
+}
+*/
+
+/*
+let random = document.getElementById("random");
+
+window.addEventListener ('load', changeText) 
+
+function changeText (){
+    random.firstElementChild.innerHTML = "HEEEEEELLO"
+}
+
+*/
