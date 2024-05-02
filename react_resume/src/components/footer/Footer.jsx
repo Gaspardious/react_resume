ccdc2c8e9a51b3e54fac2833f026a93cd3d6c07c
Dejan Gaspar <107142760+Gaspardious@users.noreply.github.com>
2024-05-02 21:47:20 +0200

    first commit

---
+++ react_resume/src/components/footer/Footer.jsx
@@ -0,0 +1,18 @@
+import styles from './footer.module.css'
+
+
+
+const Footer = () => {
+  return (
+    <>
+    <footer>
+        <div>
+        <p>© Made by Dejan Gaspar - 2024</p>
+        </div>
+       
+    </footer>
+</>
+  )
+}
+
+export default Footer
