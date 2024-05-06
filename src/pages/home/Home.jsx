0f71b36e3f8d113e64fc795e200779369177e1ee
Dejan Gaspar <107142760+Gaspardious@users.noreply.github.com>
2024-05-06 21:15:00 +0200

    last changes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -10,22 +10,17 @@ const Home = () => {
         <h1 className={styles.title}>FRONTEND DEVELOPER</h1>
         <p className={styles.text}>Hi, I'm Dejan Gašpar. A passionate Front-End Developer based in Gothenburg, Sweden. 📍</p>
           <div className={styles.tech}>  
-                      <h2 className={styles.tech_stack}>TECH STACK</h2>
-                      <h2> | </h2>
-                        <ul className={styles.techlist}>
-                          <img src="https://skillicons.dev/icons?i=html,css,js,react,sass,figma,github" alt="skill-icon" className={styles.techstack} />
-                        </ul>
-           
+              <h2 className={styles.tech_stack}>TECH STACK</h2>
+              <h2> | </h2>
+                <ul className={styles.techlist}>
+                  <img src="https://skillicons.dev/icons?i=html,css,js,react,sass,figma,github" alt="skill-icon" className={styles.techstack} />
+                </ul>
             </div>
-            <a href="https://github.com/Gaspardious" target="_blank">
-                          <img src="images/github-142-svgrepo-com.svg" alt="github" className={styles.github} />
-                        </a>
-        
+              <a href="https://github.com/Gaspardious" target="_blank">
+                <img src="images/github-142-svgrepo-com.svg" alt="github" className={styles.github} />
+              </a>
       </section>
-
-
-      <img className={styles.hero_img} src="images/Dejan.png" alt="" />
-
+        <img className={styles.hero_img} src="images/Dejan.png" alt="" />
     </div>
   )
 }
