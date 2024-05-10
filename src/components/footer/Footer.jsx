aeeb404843c95b9038ce2ea197ae1cc6aaee6dd6
Dejan Gaspar <107142760+Gaspardious@users.noreply.github.com>
2024-05-10 12:45:10 +0300

    small changes added

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,10 +6,20 @@ const Footer = () => {
   return (
     <>
     <footer className={styles.footer}>
-        <div>
+      <div>
+        <a href="https://github.com/Gaspardious" target="_blank">
+          <img src="images/github-mark.png" alt="github" className={styles.social} />
+        </a>
+        <a href="https://www.linkedin.com/in/dejan-gaspar-23800059/" target="_blank">
+          <img src="images/icons8-linkedin.svg" alt="linkedin" className={styles.social} />
+        </a>
+        <a href="https://www.instagram.com/dgaspar88/" target="_blank">
+          <img src="images/instagram-svgrepo-com.svg" alt="instagram" className={styles.social} />
+        </a>
+      </div>
+      <div>
         <p className={styles.paragraph}> © Made by Dejan Gaspar - 2024</p>
         </div>
-       
     </footer>
 </>
   )
